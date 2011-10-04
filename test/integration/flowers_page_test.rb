610ef680708483c763b733cc875f8d07e67b185c
unknown <blazingpair@.(none)>
2011-10-03 20:50:03 -0700

    flower page test

--- test/integration/flowers_page_test.rb
+++ test/integration/flowers_page_test.rb
@@ -1,23 +1,14 @@
 require "test_helper"
 
 class FlowersPageTest < ActionDispatch::IntegrationTest
-
-  # Called before every test method runs. Can be used
-  # to set up fixture information.
-  def setup
-    # Do nothing
+  def test_has_price
+    flower = Flower.create(:name => 'Columbine', :price => 99)
+    visit(flowers_path)
+    assert page.has_content?("99"), "99 appears on index page"
   end
-
-  # Called after every test method runs. Can be used to tear
-  # down fixture information.
-
-  def teardown
-    # Do nothing
-  end
-
-  # Fake test
   def test_has_name
     flower = Flower.create(:name => 'Columbine', :price => 99)
-    assert page.has_content?("Columbine")
+    visit(flowers_path)
+    assert page.has_content?("Columbine"), "Columbine appears on index page"
   end
 end
